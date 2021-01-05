d9ca1f0ee3a77e88336717b4439d5fb4d34543b5
en1tan <asajuenitan@gmail.com>
2021-01-05 12:18:01 +0100

    Log Day 1: Learnt all aboust Strings, string interpolation and how to use the Math, Console, and DateTime methods

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FirstName = "Michael";
+            string LastName = "Asaju";
+            DateTime dateTime = new DateTime();
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(FirstName + " " + LastName);
+            Console.WriteLine(1101);
+            Console.WriteLine(1001);
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(Math.Sqrt(12345));
+            Console.Write("What's your age: ");
+            int Age = int.Parse(Console.ReadLine()) + dateTime.AddYears(9).Year;
+            Console.WriteLine("You will be {0} years old", Age);
+
+        }
+    }
+}
