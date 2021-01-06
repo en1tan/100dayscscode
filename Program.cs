1017ecf6a904c3f15527534a4d261a1f72d7e7da
en1tan <asajuenitan@gmail.com>
2021-01-06 20:15:11 +0100

    📦 NEW(Log 2): This log is about data types and data manipulation, representation

--- Program.cs
+++ Program.cs
@@ -7,26 +7,87 @@ namespace tutorial
 
         static void Main(string[] args)
         {
-
-            string FirstName = "John";
-            string LastName = "Doe";
-            DateTime dateTime = new DateTime();
-
-            Console.WriteLine("Hello C#!");
-            Console.WriteLine(FirstName + " " + LastName);
-            Console.WriteLine(1101);
-            Console.WriteLine(1001);
-            Console.WriteLine(DateTime.Now);
-            Console.WriteLine(Math.Sqrt(12345));
-            for (int i = 0; i < 101; i++)
+            float a = 0.01f;
+            float b = 0.02f;
+            int hexaNum = 0x100;
+            char myChar = '\u0072';
+            bool isMale;
+            string gender = "male";
+            string firstWord = "Hello";
+            string secondWord = "World";
+            object theWord;
+            string withQuote = "The \"use\" of quotations causes difficulties";
+            string withoutQuote = "The use of quotations causes difficulties";
+            int firstNum = 5;
+            int secondNum = 10;
+            int numToSwap = firstNum;
+            firstNum = secondNum;
+            secondNum = numToSwap;
+            // Question 1
+            Console.WriteLine("sbyte: {0}, {1}, {2}", -115, 112, -44);
+            Console.WriteLine("byte: {0}, {1}", 97, 224);
+            Console.WriteLine("short: {0}", -10000, 1990);
+            Console.WriteLine("ushort: {0}", 52130);
+            Console.WriteLine("int: {0}, {1}", -1000000, 4825932);
+            Console.WriteLine("uint: {0}", 970700000);
+            Console.WriteLine("long: {0}", 123456789123456789);
+            //    Question 2
+            Console.WriteLine("Float: {0}f,{1}f", 5, 12.345);
+            Console.WriteLine("Double: {0}f, {1}f", -5.01, 8923.1234857);
+            Console.WriteLine("Decimal: {0}f, {1}f", 34.567839023, 3456.091124875956542151256683467);
+            // Question 3
+            bool equal = Math.Abs(a - b) < 0.000001;
+            Console.WriteLine("Equal?: {0}", equal);
+            // Question 4
+            Console.WriteLine(hexaNum);
+            // Question 5
+            Console.WriteLine(myChar);
+            // Question 6
+            if (gender == "male")
             {
-                Console.WriteLine(i);
+                isMale = true;
+                Console.WriteLine(isMale);
             }
-            Console.Write("What's your age: ");
-            int Age = int.Parse(Console.ReadLine()) + dateTime.AddYears(9).Year;
-            Console.WriteLine("C# is a programming language while .NET Framework is a framework built on C# for running .NET code");
-            Console.WriteLine("Python, Java, Javascript, PHP, Ruby, C++"); 
-            Console.WriteLine("You will be {0} years old", Age);
+            // Question 7
+            theWord = firstWord + " " + secondWord;
+            Console.WriteLine(theWord);
+            // Question 8
+            string otherWord = theWord.ToString();
+            Console.WriteLine(otherWord);
+
+            // Question 9
+            Console.WriteLine(withoutQuote);
+            Console.WriteLine(withQuote);
+
+            // Question 10
+            Console.WriteLine("    o       o");
+            Console.WriteLine("   ooo     ooo");
+            Console.WriteLine("  ooooo   ooooo");
+            Console.WriteLine(" ooooooo oooooooo");
+            Console.WriteLine("oooooooooooooooooo");
+            Console.WriteLine("oooooooooooooooooo ");
+            Console.WriteLine("oooooooooooooooooo ");
+            Console.WriteLine(" oooooooooooooooo ");
+            Console.WriteLine("  oooooooooooooo ");
+            Console.WriteLine("   oooooooooooo ");
+            Console.WriteLine("    oooooooooo ");
+            Console.WriteLine("     oooooooo ");
+            Console.WriteLine("      oooooo ");
+            Console.WriteLine("       oooo ");
+            Console.WriteLine("        oo ");
+
+            // Question 11
+            Console.WriteLine("          ©");
+            Console.WriteLine("         ©©©");
+            Console.WriteLine("        ©©©©©");
+            Console.WriteLine("       ©©©©©©©");
+            Console.WriteLine("      ©©©©©©©©©");
+            Console.WriteLine("     ©©©©©©©©©©©");
+            Console.WriteLine("    ©©©©©©©©©©©©©");
+            Console.WriteLine("   ©©©©©©©©©©©©©©©");
+            Console.WriteLine("  ©©©©©©©©©©©©©©©©©");
+            Console.WriteLine(" ©©©©©©©©©©©©©©©©©©©");
+            Console.WriteLine("©©©©©©©©©©©©©©©©©©©©©");
         }
     }
 }
