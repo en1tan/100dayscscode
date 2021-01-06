024f4ab86a37b9c66b6bdf50c7c81c36628c6a4b
en1tan <asajuenitan@gmail.com>
2021-01-06 18:26:38 +0100

    👌 IMPROVE(add): Added more questions from the questions in chapter 1

--- Program.cs
+++ Program.cs
@@ -12,16 +12,21 @@ namespace tutorial
             string LastName = "Doe";
             DateTime dateTime = new DateTime();
 
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello C#!");
             Console.WriteLine(FirstName + " " + LastName);
             Console.WriteLine(1101);
             Console.WriteLine(1001);
             Console.WriteLine(DateTime.Now);
             Console.WriteLine(Math.Sqrt(12345));
+            for (int i = 0; i < 101; i++)
+            {
+                Console.WriteLine(i);
+            }
             Console.Write("What's your age: ");
             int Age = int.Parse(Console.ReadLine()) + dateTime.AddYears(9).Year;
+            Console.WriteLine("C# is a programming language while .NET Framework is a framework built on C# for running .NET code");
+            Console.WriteLine("Python, Java, Javascript, PHP, Ruby, C++"); 
             Console.WriteLine("You will be {0} years old", Age);
-
         }
     }
 }
