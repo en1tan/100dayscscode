68c4e0a20377354aa4f4db0dee0d9f51a8c93c05
en1tan <asajuenitan@gmail.com>
2021-01-06 18:32:32 +0100

    ♻️ REFACTOR(added &amp; removed): Removed the template code and added README file for easy step by step

--- Program.cs
+++ Program.cs
@@ -6,19 +6,7 @@ namespace tutorial
     {
         static void Main(string[] args)
         {
-            string FirstName = "Michael";
-            string LastName = "Asaju";
-            DateTime dateTime = new DateTime();
-
-            Console.WriteLine("Hello World!");
-            Console.WriteLine(FirstName + " " + LastName);
-            Console.WriteLine(1101);
-            Console.WriteLine(1001);
-            Console.WriteLine(DateTime.Now);
-            Console.WriteLine(Math.Sqrt(12345));
-            Console.Write("What's your age: ");
-            int Age = int.Parse(Console.ReadLine()) + dateTime.AddYears(9).Year;
-            Console.WriteLine("You will be {0} years old", Age);
+            Console.WriteLine("Welcome to 100 Days CSharp Code Challenge");
 
         }
     }
---
+++ README.md
@@ -0,0 +1,6 @@
+# 100 Days of C# Code Challenge
+
+The main branch doesn't have much so be sure to checkout the branches
+I will continually update this README everyday and create a branch for everyday
+
+Made with 💓 by [x1k](https://strae.dev)
